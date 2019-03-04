7981e86cef43544b56815f733b8d32053eb46096
mwarck <32462379+mwarck@users.noreply.github.com>
2019-03-04 10:03:17 -0600

    plugin customized to show inputs values on shop page

---
+++ quantity-field-on-shop-page-for-woocommerce/wc-quantity-field-shop-page.php
@@ -0,0 +1,164 @@
+<?php
+/*
+Plugin Name: Quantity Field on Shop Page for WooCommerce
+Plugin URI:  http://wooassist.com
+Description: Adds a ‘Quantity Field’ on the shop and category pages of your Woocommerce store. This allows the user to change the quantity of a product before adding it to the cart.
+Version:     1.1.0
+Author:      Wooassist
+Author URI:  http://wooassist.com
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+if ( ! class_exists( 'woa_wqfsp' ) ) :
+
+class woa_wqfsp {
+
+	/**
+	 * @var The single instance of the class
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Main woa_wqfsp Instance
+	 *
+	 * Ensures only one instance of WooCommerce is loaded or can be loaded.
+	 *
+	 * @static
+	 * @see woa_wqfsp()
+	 * @return woa_wqfsp main instance
+	 */
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * woa_wqfsp Constructor.
+	 */
+	public function __construct() {
+     $this->textfield_id = 'unit_txt_field';
+		add_action( 'wp_enqueue_scripts', array( $this, 'woa_add_quantity_style' ) );
+		add_action( 'init', array( $this, 'woa_quantity_handler' ) );
+		add_action( 'init', array( $this, 'woa_confirm_add' ) );
+    add_filter( 'woocommerce_quantity_input_args', array($this, 'add_quantity_args'),10,2);
+		add_filter( 'woocommerce_loop_add_to_cart_link', array( $this, 'woa_add_quantity_fields' ), 10, 2 );
+
+
+	}
+
+	/**
+	 * Add quantity fields.
+	 */
+	public function woa_add_quantity_fields($html, $product) {
+    $meta_unit = get_post_meta( get_the_ID(), $this->textfield_id,true);
+		//add quantity field only to simple products
+	if ( $product && $product->is_type( 'simple' ) && $product->is_purchasable() && $product->is_in_stock() && ! $product->is_sold_individually() ) {
+		//rewrite form code for add to cart button
+		//rewrite form code for add to cart button
+      $id= $product->id;
+		$html = '<form action="' . esc_url( $product->add_to_cart_url() ) .'"id="'.$id.'" class="cart" method="post" enctype="multipart/form-data">';
+		$html .= woocommerce_quantity_input( array(), $product, false );
+    $html .='<div><input type="text" readonly style="width:33%;" name="product_id" id="base_' .$id. '" value=""/><a>gramos</a></div>';
+    $html .='<input type="hidden" name="calculus_'.$id. '"  value="' .$meta_unit. '"class="calculus"/>';
+		$html .= '<button type="submit" data-quantity="1" data-product_id="' . $product->id . '" class="button alt ajax_add_to_cart add_to_cart_button product_type_simple">' . esc_html( $product->add_to_cart_text() ) . '</button>';
+		$html .= '</form>';
+	}
+	return $html;
+	}
+  /**
+   * [add_quantity_args description]
+   * addingthe id product to each quantity input value for j s
+   * @param [type] $args    [description]
+   * @param [type] $product [description]
+   */
+  public function add_quantity_args($args, $product){
+    $id= $product->id;
+    $args['name'] = "'qty_'.$id";
+    return $args;
+  }
+
+	/**
+	 * woa_wqfsp add stylesheet for quantity field.
+	 */
+	public function woa_add_quantity_style() {
+
+		wp_enqueue_style( 'WQFSP_style', plugins_url( '/css/style.css', __FILE__ ) );
+
+	}
+
+	/**
+	 * add AJAX support.
+	 * synchs quantity field
+	 */
+	public function woa_quantity_handler() {
+		wc_enqueue_js( '
+		jQuery(function($) {
+		$("form.cart").on("change", "input.qty", function() {
+        $(this.form).find("[data-quantity]").data("quantity", this.value);
+		});
+		' );
+
+		wc_enqueue_js( '
+		$(document.body).on("adding_to_cart", function() {
+			$("a.added_to_cart").remove();
+		});
+		});
+		' );
+	}
+
+	/**
+	 * add checkmark
+	 *
+	 */
+	public function woa_confirm_add() {
+		wc_enqueue_js( '
+		jQuery(document.body).on("added_to_cart", function( data ) {
+
+		jQuery(".added_to_cart").after("<p class=\'confirm_add\'>Item Added</p>");
+});
+
+		' );
+	}
+
+}
+
+endif; // ! class_exists()
+
+/**
+ * Returns the main instance of woa_wqfsp.
+ */
+function woa_wqfsp_run() {
+	return woa_wqfsp::instance();
+}
+
+/**
+ * WC Detection
+ *
+ * @since  1.5.4
+ * @return boolean
+ */
+if ( ! function_exists( 'woa_is_woocommerce_active' ) ) {
+	function woa_is_woocommerce_active() {
+		$active_plugins = (array) get_option( 'active_plugins', array() );
+
+		if ( is_multisite() ) {
+			$active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
+		}
+
+		return in_array( 'woocommerce/woocommerce.php', $active_plugins ) || array_key_exists( 'woocommerce/woocommerce.php', $active_plugins ) ;
+	}
+}
+
+/*
+ * Initialize
+ */
+if ( woa_is_woocommerce_active() ) {
+
+	woa_wqfsp_run();
+
+}
